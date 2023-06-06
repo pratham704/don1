26c680cfad398868380509693c96a084342c5217
pratham704 <125141594+pratham704@users.noreply.github.com>
2023-06-06 22:23:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const cors = require('cors')
+const abc = require('./contactmodel');
 
 const app = express();
 const dotenv = require('dotenv')
@@ -7,6 +10,13 @@ dotenv.config();
 const { resolve } = require('path');
 app.use(cors());
 app.use(express.json());
+
+mongoose.set('strictQuery', false);
+
+const url = `${process.env.MONGO}`
+
+
+mongoose.connect(url)
 
 
 
@@ -24,6 +34,31 @@ app.post('/regi', async(req, res) => {
 
     res.json("done")
 
+
+
+})
+
+
+
+app.post('/reg', async(req, res) => {
+
+
+    const resn = await abc.findOne({ username: req.body.username })
+    if (resn !== null) {
+
+        res.json("alreadyexist")
+
+
+    } else {
+        const data = await abc.create({
+            username: req.body.username,
+            password: req.body.password,
+
+        })
+
+
+        res.json("success")
+    }
 
 
 })
