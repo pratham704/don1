a10a019369e3c33bd8b69b901b9082b1eafe2f6f
pratham704 <125141594+pratham704@users.noreply.github.com>
2023-06-06 22:39:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const cors = require('cors')
 const abc = require('./contactmodel');
-
 const app = express();
 const dotenv = require('dotenv')
 dotenv.config();
