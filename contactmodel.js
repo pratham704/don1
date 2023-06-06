e0a166f75691a6ab1c92316f7d1ad5a0000a81fa
pratham704 <125141594+pratham704@users.noreply.github.com>
2023-06-06 22:44:51 +0530

    Create contactmodel.js

---
+++ contactmodel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+    },
+    password: {
+        type: String,
+    },
+    requestfrompeople: {
+        type: [String],
+    },
+
+    acceptedbyme: {
+        type: [String],
+    },
+
+    bio: {
+        type: String,
+        default: 'no bio added',
+    },
+}, { timestamps: true });
+
+
+const UserModel = mongoose.model('contachInfo', UserSchema);
+
+module.exports = UserModel;
